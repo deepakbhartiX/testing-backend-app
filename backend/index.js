710fa14753587bac1bff04739e45a8ed48ac612e
deepakbhartiX <deepak17122002@gmail.com>
2024-11-10 20:52:13 +0530

    some chanes in "about"

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-console.log('heelo')
+import express from "express"
+import 'dotenv/config'
+const app = express()
+
+
+const port = 3000
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>lol</h1>')
+    
+})
+
+
+app.get('/about',(req,res)=>{
+    res.send('this is me deepak bharti me')
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`app listening on part ${process.env.PORT} `)
+})
+
